50add470a8dc889ae96f588ce2fb92eb2a849cee
litus <cfar@t2client.com>
2021-01-07 17:33:53 +0100

    initial project vue3 + install/config tailwindcss & firebase

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>Project</div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "fireabase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDigFRsFXCnZloCyk4GkVRjnQ-XBDbzUmI",
+    authDomain: "idea-box-1f66f.firebaseapp.com",
+    projectId: "idea-box-1f66f",
+    storageBucket: "idea-box-1f66f.appspot.com",
+    messagingSenderId: "805771480593",
+    appId: "1:805771480593:web:0a2e93f9389ca701f0ebb0"
+  };
+
+firebase.initialApp(firebaseConfig);
