16fa45109e490497c763c9d06b528735f726ed40
litus <cfar@t2client.com>
2021-01-08 00:06:12 +0100

    add auth & methods login - logout

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,14 @@
 <section class="mb-6">
       <form class="sm:flex">
         <input class="w-full p-3 sm:flex-auto" type="text" required placeholder="Add your idea"/>
-        <input class="w-full p-2 bg-gray-600 text-white sm:flex-1" type="submit" value="Add idea"/>
+        <input v-if="user" class="w-full p-2 bg-gray-600 text-white sm:flex-1" type="submit" value="Add idea"/>
       </form>
+      <p class="users-actions" v-if="!user">
+        Please <a @click="doLogin" href="#">login</a> first
+      </p>
+      <p class="users-actions" v-else> 
+        Hi 👋{{ user.displayName }}.  <a @click="doLogout" href="#">logout</a>
+      </p>
     </section>
 
 
@@ -28,6 +34,7 @@
 import AppIdea from '@/components/AppIdea';
 import seed from "@/seed.json";
 import { ref } from "vue";
+import { auth, firebase } from "@/firebase.js";
 
 export default {
   name: "App",
@@ -36,13 +43,43 @@ export default {
   },
   setup(){
     const ideas = ref(seed.ideas);
+    let user = ref(null);
 
-    return { ideas }
+    auth.onAuthStateChanged(async auth => (user.value = auth ?  auth : null));
+
+    const doLogin = async ()=>{
+      const provider = new firebase.auth.GoogleAuthProvider();
+
+      try{
+        await auth.signInWithPopup(provider);
+
+      }catch(error){
+        console.error(error);
+      }
+    }
+
+    const doLogout = async ()=>{
+      try{
+        await auth.signOut();
+
+      }catch(error){
+        console.error(error);
+      }
+
+    }
+
+    return { ideas, user, doLogin, doLogout}
 
   }
 };
 </script>
 
 <style>
-
+.users-actions{
+  @apply mt-2 text-center;
+}
+.users-actions a{
+  font-weight: bold;
+  text-decoration: underline;
+}
 </style>
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,18 @@
-import firebase from "fireabase/app";
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDigFRsFXCnZloCyk4GkVRjnQ-XBDbzUmI",
-    authDomain: "idea-box-1f66f.firebaseapp.com",
-    projectId: "idea-box-1f66f",
-    storageBucket: "idea-box-1f66f.appspot.com",
-    messagingSenderId: "805771480593",
-    appId: "1:805771480593:web:0a2e93f9389ca701f0ebb0"
-  };
+  apiKey: "AIzaSyDigFRsFXCnZloCyk4GkVRjnQ-XBDbzUmI",
+  authDomain: "idea-box-1f66f.firebaseapp.com",
+  projectId: "idea-box-1f66f",
+  storageBucket: "idea-box-1f66f.appspot.com",
+  messagingSenderId: "805771480593",
+  appId: "1:805771480593:web:0a2e93f9389ca701f0ebb0"
+};
 
-firebase.initialApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+export { auth, firebase};
