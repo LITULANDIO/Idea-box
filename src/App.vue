e7d85d87cce46dee83aaaea62199264aa006e538
litus <cfar@t2client.com>
2021-01-07 19:01:31 +0100

    create cmp AppIdea & inject json ideas

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,15 @@
     <h1 class="mb-5 text-4xl text-center">Ideabox</h1>
 
 <!--Add idea-->
-    <section class="mb-6">
+<section class="mb-6">
       <form class="sm:flex">
         <input class="w-full p-3 sm:flex-auto" type="text" required placeholder="Add your idea"/>
         <input class="w-full p-2 bg-gray-600 text-white sm:flex-1" type="submit" value="Add idea"/>
       </form>
     </section>
 
-<!--Idea item-->
-    <article class="p-3 rounded-lg bg-gray-300 sm:flex sm:items-center">
-      <section class="text-center sm:flex-1 sm:text-left">
-        <h2 class="text-xl sm:leading-6 sm:text-2xl">This is idea title and must be collpases on another row</h2>
-        <small>Username</small>
-      </section>
 
-<!--Vote-->
-      <section class="pt-3 border-t-2 mt-6 border-black sm:pt-0 sm:pl-3 sm:border-t-0 sm:border-l-2 sm:mt-0 sm:flex sm:items-center">
-        <h3 class="text-3xl font-bold text-center ml-3 mr-3">99</h3>
-        <nav class="flex justify-center sm:block">
-          <img class="w-10 cursor-pointer" src="./assets/arrow-up.svg" alt="Vote up" />
-          <img class="w-10 cursor-pointer transform rotate-180" src="./assets/arrow-up.svg" alt="Vote down" />
-        </nav>
-      </section>
-    </article>
+    <AppIdea v-for="(idea, $index) in ideas" :key="$index" :idea="idea"/>
 
 
   </div>
@@ -39,10 +25,20 @@
 </template>
 
 <script>
+import AppIdea from '@/components/AppIdea';
+import seed from "@/seed.json";
+import { ref } from "vue";
 
 export default {
   name: "App",
   components: {
+    AppIdea
+  },
+  setup(){
+    const ideas = ref(seed.ideas);
+
+    return { ideas }
+
   }
 };
 </script>
